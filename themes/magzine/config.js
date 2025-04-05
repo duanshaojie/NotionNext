836ae11cfd50ea56c9f3a8c3c79a93ec4a601a2b
小四 <Edison_dsj@163.com>
2025-04-05 16:26:53 +0800

    Update config.js

--- themes/magzine/config.js
+++ themes/magzine/config.js
@@ -1,16 +1,16 @@
 const CONFIG = {
   // 首屏信息栏按钮文字
-  MAGZINE_HOME_BANNER_ENABLE: false, // 首屏右上角的宣传位
+  MAGZINE_HOME_BANNER_ENABLE: true, // 首屏右上角的宣传位
   MAGZINE_HOME_BUTTON: true,
   MAGZINE_HOME_BUTTON_URL: '/about',
   MAGZINE_HOME_BUTTON_TEXT: '了解更多',
 
   MAGZINE_HOME_HIDDEN_CATEGORY: '分享杂文', //不希望在首页展示的文章分类，用英文逗号隔开
 
-  MAGZINE_HOME_TITLE: '立即开创您的在线业务。完全免费。',
+  MAGZINE_HOME_TITLE: '欢迎加入我的医疗行业社群',
   MAGZINE_HOME_DESCRIPTION:
-    '借助NotionNext，获得助您开创、经营和扩展业务所需的全部工具和帮助。',
-  MAGZINE_HOME_TIPS: 'AI时代来临，这是属于超级个体的狂欢盛宴！',
+    '分享交流最新的AI技术，交换最新的医疗行业信息。',
+  MAGZINE_HOME_TIPS: '为了防止世界被破坏，为了维护世界的和平',
 
   // 首页底部推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   MAGZINE_RECOMMEND_POST_TAG: '推荐',
